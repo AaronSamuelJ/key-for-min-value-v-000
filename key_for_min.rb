a4e7afb57af3bd7e51c5cbd867455085db32ea31
Aaron Samuel <aasamuelj@gmail.com>
2017-09-26 06:25:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
-a.values.min_by(&:last)
-hash.values.min => 3
+# a.values.min_by(&:last)
+# hash.values.min => 3
 
 def key_for_min_value(hash)
-  num = hash.values.min 
+  num = hash.values.min
   hash.each do |k, v|
     if v == 3
       k
